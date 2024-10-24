3dc42d0ef27926e5b871ba5210181e407bff9aaf
Brando Vidal <yisusdracon@gmail.com>
2024-10-24 11:11:50 -0500

    build: add core web vitals and db

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,17 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import webVitals from "@astrojs/web-vitals";
+
+import db from "@astrojs/db";
+
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), webVitals(), db()],
+  output: 'hybrid',
+  adapter: node({
+    mode: "standalone"
+  })
 });
