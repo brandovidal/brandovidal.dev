4779f2f450d2cb6e72b9a675011098e7873d42a6
Brando Vidal <yisusdracon@gmail.com>
2024-10-24 11:19:58 -0500

    build: add cloudflare adapter to deploy site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,11 @@ import webVitals from "@astrojs/web-vitals";
 
 import db from "@astrojs/db";
 
-import node from "@astrojs/node";
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), webVitals(), db()],
   output: 'hybrid',
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: cloudflare()
 });
